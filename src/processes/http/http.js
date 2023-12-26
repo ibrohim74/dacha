cced50ef4b8e9f45a9651182c3e47205c19371aa
ibrohim74 <xibroxim@gmail.com>
2023-12-27 00:00:50 +0500

    asd

--- src/processes/http/http.js
+++ src/processes/http/http.js
@@ -4,7 +4,7 @@ const $host = axios.create({
     baseURL: "https://my-json-server.typicode.com/ibrohim74/dacha/",
 });
 const $authHost = axios.create({
-    baseURL: "https://dacha-ecru.vercel.app/api",
+    baseURL: "https://my-json-server.typicode.com/ibrohim74/dacha/",
 });
 
 export {$host , $authHost}
