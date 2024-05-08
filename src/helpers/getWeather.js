69a5566694998ccfb2650a3b388b3abf4137996f
Mali <keeroh@gmail.com>
2024-05-08 15:29:38 +0500

    weather fetching fix

--- src/helpers/getWeather.js
+++ src/helpers/getWeather.js
@@ -3,7 +3,11 @@ import { WEATHER_BASE_URL } from "../processes/utils/consts";
 const apiKey = "21c332391c804785aa885105240605";
 
 export async function getWeather(latitude, longitude) {
-  const url = `${WEATHER_BASE_URL}?key=${apiKey}&q=${latitude},${longitude}`;
+  //   const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${latitude},${longitude}`;
+
+  // WEATHER_BASE_URL = "http://api.weatherapi.com/v1/current.json";
+
+  const url = `${WEATHER_BASE_URL}??key=${apiKey}&q=${latitude},${longitude}`;
 
   try {
     const response = await fetch(url);
